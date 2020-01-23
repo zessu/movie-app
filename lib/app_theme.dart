443ffb6d5379dbec1d7e0b5880486237781775db
zessu <amackzie@gmail.com>
2020-01-23 12:31:38 +0300

    (style): define text styles

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -6,18 +6,20 @@ class AppTheme {
   static const Color _lightSecondaryColor = Colors.red;
   static const Color _lightOnPrimaryColor = Colors.black;
   static const Color _lightOnSecondaryColor = Colors.black;
-  static const Color _lightBackGroundColor = Colors.grey;
+  static const Color _lightBackGroundColor = Colors.white;
   static const Color _lightSurfaceColor = Colors.white;
+  static const Color _lightOnSurfaceColor = Colors.black;
 
   // dark theme colors
   static const Color _darkPrimaryColor = Colors.yellow;
-  static const Color _darkSecondaryColor = Colors.black;
+  static const Color _darkSecondaryColor = Colors.blue;
   static const Color _darkOnPrimaryColor = Colors.black;
-  static const Color _darkOnSecondaryColor = Colors.black;
+  static const Color _darkOnSecondaryColor = Colors.white;
   static const Color _darkBackGroundColor = Colors.black54;
-  static const Color _darkSurfaceColor = Colors.grey;
+  static const Color _darkSurfaceColor = Colors.black54;
+  static const Color _darkOnSurfaceColor = Colors.white;
 
-  static const Color _errorColor = Colors.red;
+  static const Color _errorColor = Colors.redAccent;
 
   static final ThemeData lightTheme = ThemeData(
     scaffoldBackgroundColor: _lightSurfaceColor,
@@ -32,7 +34,8 @@ class AppTheme {
         onSecondary: _lightOnSecondaryColor,
         surface: _lightSurfaceColor,
         background: _lightBackGroundColor,
-        error: _errorColor),
+        error: _errorColor,
+        onSurface: _lightOnSurfaceColor),
     textTheme: _lightTextTheme,
   );
 
@@ -49,35 +52,38 @@ class AppTheme {
         onSecondary: _darkOnSecondaryColor,
         surface: _darkSurfaceColor,
         background: _darkBackGroundColor,
-        error: _errorColor),
+        error: _errorColor,
+        onSurface: _darkOnSurfaceColor),
     textTheme: _darkTextTheme,
   );
 
+  static final TextStyle _lightScreenTitleTextStyle =
+      TextStyle(fontSize: 24.0, color: _lightOnSurfaceColor);
+  static final TextStyle _lightScreenSubTitleTextStyle =
+      TextStyle(fontSize: 16.0, color: _lightOnSurfaceColor);
   static final TextStyle _lightScreenHeadingTextStyle =
-      TextStyle(fontSize: 48.0, color: _lightOnPrimaryColor);
-  static final TextStyle _lightScreenTaskNameTextStyle =
-      TextStyle(fontSize: 20.0, color: _lightOnPrimaryColor);
-  static final TextStyle _lightScreenTaskDurationTextStyle =
-      TextStyle(fontSize: 16.0, color: Colors.grey);
+      TextStyle(fontSize: 48.0, color: _lightOnSurfaceColor);
+  static final TextStyle _lightScreenBodyTextStyle =
+      TextStyle(fontSize: 20.0, color: _lightOnSurfaceColor);
 
+  static final TextStyle _darkScreenTitleTextStyle =
+      TextStyle(fontSize: 24.0, color: _darkOnSurfaceColor);
+  static final TextStyle _darkScreenSubTitleTextStyle =
+      TextStyle(fontSize: 16.0, color: _darkOnSurfaceColor);
   static final TextStyle _darkScreenHeadingTextStyle =
-      _lightScreenHeadingTextStyle.copyWith(color: _darkOnPrimaryColor);
-
-  static final TextStyle _darkScreenTaskNameTextStyle =
-      _lightScreenTaskNameTextStyle.copyWith(color: _darkOnPrimaryColor);
-
-  static final TextStyle _darkScreenTaskDurationTextStyle =
-      _lightScreenTaskDurationTextStyle;
+      TextStyle(fontSize: 48.0, color: _darkOnSurfaceColor);
+  static final TextStyle _darkScreenBodyTextStyle =
+      TextStyle(fontSize: 20.0, color: _darkOnSurfaceColor);
 
   static final TextTheme _lightTextTheme = TextTheme(
-    headline: _lightScreenHeadingTextStyle,
-    body1: _lightScreenTaskNameTextStyle,
-    body2: _lightScreenTaskDurationTextStyle,
-  );
+      title: _lightScreenTitleTextStyle,
+      subtitle: _lightScreenSubTitleTextStyle,
+      headline: _lightScreenHeadingTextStyle,
+      body1: _lightScreenBodyTextStyle);
 
   static final TextTheme _darkTextTheme = TextTheme(
-    headline: _darkScreenHeadingTextStyle,
-    body1: _darkScreenTaskNameTextStyle,
-    body2: _darkScreenTaskDurationTextStyle,
-  );
+      title: _darkScreenTitleTextStyle,
+      subtitle: _darkScreenSubTitleTextStyle,
+      headline: _darkScreenHeadingTextStyle,
+      body1: _darkScreenBodyTextStyle);
 }
